a93847851fee27e1a35ae83f1a6980fc3728a66b
Nikita Demyanchuk <86979292+nikiniki1@users.noreply.github.com>
2023-05-10 16:30:43 +0300

    lambdas compute update
    Solved the problem of mismatched lambdas.

--- PINN_NTK_Wave_1D_torch.py
+++ PINN_NTK_Wave_1D_torch.py
@@ -83,16 +83,27 @@ class Solver():
         return torch.cat([x.reshape(-1) for x in list_])
 
     def jacobian(self, f):
-        jac = []
-        for i in range(len(f)):
-            f[i].backward(retain_graph=True)
-            deriv = [w.grad.reshape(-1).to(device) if w.grad is not None else torch.tensor([0]).to(device) for w in self.model.parameters()]
-            jac.append(self.list_to_vector(deriv))
-        jac = torch.vstack(jac)
+        jac = {}
+        for name, param in self.model.named_parameters():
+            jac1 = []
+            for op in f:
+                grad, = torch.autograd.grad(op, param, retain_graph=True, allow_unused=True)
+                if grad is None:
+                    grad = torch.tensor([0.])
+                jac1.append(grad.reshape(1,-1))
+            jac[name] = torch.cat(jac1)
         return jac
 
     def compute_ntk(self, J1, J2):
-        return J1 @ J2.T
+        keys = list(J1_dict.keys())
+        size = J1_dict[keys[0]].shape[0]
+        Ker = torch.zeros((size, size))
+        for key in keys:
+            J1 = J1_dict[key]
+            J2 = J2_dict[key]
+            K = J1 @ J2.T
+            Ker = Ker + K
+        return Ker
 
     def loss_bnd_op(self, X_bcs_1, X_bcs_2, X_ics, y_ics):
         bnd1 = self.model(X_bcs_1)
